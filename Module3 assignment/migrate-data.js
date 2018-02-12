88ee24630c7ee4fc415339a4078c2e05f385bbde
vignselv <vigneshwaran.selvaraj@capgemini.com>
2018-02-12 11:14:09 +0530

    module3 assignment

---
+++ Module3 assignment/migrate-data.js
@@ -0,0 +1,36 @@
+const mongodb= require('mongodb')
+const url = 'mongodb://localhost:27017'
+const customers = require('./customer-data.json')
+const customerAddresses = require('./customer-address-data.json')
+const async = require('async')
+
+let tasks = []
+const limit = parseInt(process.argv[2], 10) || 1000
+mongodb.MongoClient.connect(url, (error, Client) => {
+
+    var db= Client.db('mydatabase');
+  if (error) return process.exit(1)
+  customers.forEach((customer, index, list) => {
+    customers[index] = Object.assign(customer, customerAddresses[index])
+
+    if (index % limit == 0) {
+      const start = index
+      const end = (start+limit > customers.length) ? customers.length-1 : start+limit
+      tasks.push((done) => {
+        console.log(`Processing ${start}-${end} out of ${customers.length}`)
+        db.collection('customers').insert(customers.slice(start, end), (error, results) => {
+          done(error, results)
+        })
+      })
+    } 
+  })
+  console.log(`Launching ${tasks.length} parallel task(s)`)
+  const startTime = Date.now()
+  async.parallel(tasks, (error, results) => {
+    if (error) console.error(error)
+    const endTime = Date.now()
+    console.log(`Execution time: ${endTime-startTime}`)
+    // console.log(results)
+    Client.close()
+  })
+})
